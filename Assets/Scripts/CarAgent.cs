db72ce94728a3db4b1bce45bf75552d40d701806
Krishna Manoj <km69564@usc.edu>
2021-02-04 19:49:50 -0800

    First Commit

---
+++ Assets/Scripts/CarAgent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class CarAgent : Agent
+{
+    public override void OnActionReceived(float[] actions)
+    {
+        Debug.Log(actions);
+    }
+
+    [SerializeField] private Transform targetTransform;
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
+}
